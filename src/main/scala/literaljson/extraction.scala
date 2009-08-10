b49b685f6eba1202633831ccc52935d98eb32e5d
Joni Freeman <joni.freeman@reaktor.fi>
2009-08-10 23:40:12 +0300

    Simplify building of case class instance from Mapping.

--- src/main/scala/literaljson/extraction.scala
+++ src/main/scala/literaljson/extraction.scala
@@ -6,14 +6,30 @@ import JsonAST._
 object Extraction {
   /** Intermediate format which describes the mapping.
    *  This ADT is constructed (and then memoized) from given case class using reflection.
+   *
+   *  Example mapping.
+   *
+   *  package xx 
+   *  case class Person(name: String, address: Address, children: List[Child])
+   *  case class Address(street: String, city: String)
+   *  case class Child(name: String, age: BigInt)
+   *
+   *  will produce following Mapping:
+   *
+   *  Constructor(None, "xx.Person", List(
+   *    Value("name"),
+   *    Constructor(Some("address"), "xx.Address", List(Value("street"), Value("city"))),
+   *    ListConstructor("children", "xx.Child", List(Value("name"), Value("age")))))
+   * 
    */
   sealed abstract class Mapping
-  case class Value(path: List[String]) extends Mapping
-  case class Constructor(classname: String, args: List[Mapping]) extends Mapping
-//  case class ListConstructor(constructors: List[Constructor]) extends Mapping
+  case class Value(path: String) extends Mapping
+  case class Constructor(path: Option[String], classname: String, args: List[Mapping]) extends Mapping
+  case class ListConstructor(path: String, classname: String, args: List[Mapping]) extends Mapping
 
   def extract[A](json: JValue)(implicit mf: Manifest[A]) = {
     val mapping = memoize(mf.erasure)
+    println(mapping)
 
     def newInstance(classname: String, args: List[Any]) = {
       val clazz = Class.forName(classname)
@@ -24,24 +40,44 @@ object Extraction {
       clazz.getConstructor(argTypes.toArray: _*).newInstance(args.map(_.asInstanceOf[AnyRef]).toArray: _*)
     }
 
-    def build(mapping: Mapping, argStack: List[Any]): List[Any] = mapping match {
-      case Value(path) => (path.foldLeft(json)(_ \ _)).asInstanceOf[JField].value.values :: argStack
-      case Constructor(classname, args) => newInstance(classname, args.flatMap(build(_, argStack))) :: Nil
-//      case ListConstructor(classname, args) => List(newInstance(classname, args.flatMap(build(_, argStack))) :: Nil)
+    def build(root: JValue, mapping: Mapping, argStack: List[Any]): List[Any] = mapping match {
+      case Value(path) => fieldValue(root, path).values :: argStack
+      case Constructor(path, classname, args) => 
+        val newRoot = path match {
+          case Some(p) => root \ p
+          case None => root
+        }
+        newInstance(classname, args.flatMap(build(newRoot, _, argStack))) :: Nil
+      case ListConstructor(path, classname, args) => 
+        val arr = fieldValue(root, path).asInstanceOf[JArray]
+        arr.arr.map(elem => newInstance(classname, args.flatMap(build(elem, _, argStack)))) :: argStack
     }
 
-    build(mapping, Nil).head
+    def fieldValue(json: JValue, path: String) = (json \ path).asInstanceOf[JField].value
+
+    build(json, mapping, Nil).head
   }
 
   // FIXME memoize
-  def memoize(clazz: Class[_]) = {
-    def makeMapping(clazz: Class[_], path: List[String]): Mapping = 
-      Constructor(clazz.getName, clazz.getDeclaredFields.map { x =>
-        if (x.getType == classOf[String]) Value(path + x.getName) // FIXME + is deprecated
-        else makeMapping(x.getType, path + x.getName) // FIXME + is deprecated
-      }.toList.reverse)
+  private def memoize(clazz: Class[_]) = {
+    def makeMapping(path: Option[String], clazz: Class[_], isList: Boolean): Mapping = isList match {
+      case false =>
+        Constructor(path, clazz.getName, clazz.getDeclaredFields.map { x =>
+          if (x.getType == classOf[String]) Value(x.getName)
+          else if (x.getType == classOf[BigInt]) Value(x.getName)
+          else if (x.getType == classOf[List[_]]) makeMapping(Some(x.getName), x.getType, true) // x.getType must be Lists type parameter
+          else makeMapping(Some(x.getName), x.getType, false)
+        }.toList.reverse)
+      case true =>
+        ListConstructor(path.get, clazz.getName, clazz.getDeclaredFields.map { x =>
+          if (x.getType == classOf[String]) Value(x.getName)
+          else if (x.getType == classOf[BigInt]) Value(x.getName)
+          else if (x.getType == classOf[List[_]]) makeMapping(Some(x.getName), x.getType, true)
+          else makeMapping(Some(x.getName), x.getType, false)
+        }.toList.reverse)
+    }
     
-    makeMapping(clazz, Nil)
+    makeMapping(None, clazz, false)
   }
 }
 
